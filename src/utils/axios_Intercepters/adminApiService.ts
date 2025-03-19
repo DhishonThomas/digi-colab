7f7465e557f43c67a2a238581dd0fef5c19fc95b
DhishonThomas <dhishonthomas23@gmail.com>
2025-03-19 22:29:22 +0530

    Axios interceptors updated .. better update with redux store...

--- src/utils/axios_Intercepters/adminApiService.ts
+++ src/utils/axios_Intercepters/adminApiService.ts
@@ -4,44 +4,41 @@ import store from "@/store/store";
 import { logout } from "@/store/slices/adminSlice";
 
 const adminApi = axios.create({
-  baseURL: ADMIN_URL, 
+  baseURL: ADMIN_URL,
   headers: { "Content-Type": "application/json" },
 });
 
 // Request Interceptor
 
 adminApi.interceptors.request.use(
-    (config) => {
-      const { token } = store.getState().admin;
-  
-      if (token) {
-        config.headers["Authorization"] = `Bearer ${token}`;
-      }
-  
-      return config;
-    },
-    (error) => {
-      return Promise.reject(error);
-    }
-  );
+  (config) => {
+    const { token } = store.getState().admin;
 
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 // Response Interceptor
 adminApi.interceptors.response.use(
-    (response) => {
-      return response;
-    },
-  
-    (error) => {
-      if (error.response && error.response.status === 401) {
-        store.dispatch(logout());
-  
-            window.location.href = "/admin/login";
-          
-      }
-  
-      return Promise.reject(new Error("Session expired. Please log in again."));
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      store.dispatch(logout());
+
+      window.location.href = "/admin/login";
     }
-  );
+
+    return Promise.reject(new Error("Session expired. Please log in again."));
+  }
+);
 
 export default adminApi;
--- src/utils/axios_Intercepters/volunteerApiService.ts
+++ src/utils/axios_Intercepters/volunteerApiService.ts
@@ -4,43 +4,41 @@ import store from "@/store/store";
 import { logout } from "@/store/slices/volunteer";
 
 const volunteerApi = axios.create({
-  baseURL: VOLUNTEER_URL, 
+  baseURL: VOLUNTEER_URL,
   headers: { "Content-Type": "application/json" },
 });
 
 // Request Interceptor
 
 volunteerApi.interceptors.request.use(
-    (config) => {
-      const { token } = store.getState().volunteer;
-  
-      if (token) {
-        config.headers["Authorization"] = `Bearer ${token}`;
-      }
-  
-      return config;
-    },
-    (error) => {
-      return Promise.reject(error);
-    }
-  );
+  (config) => {
+    const { token } = store.getState().volunteer;
 
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 // Response Interceptor
 volunteerApi.interceptors.response.use(
-    (response) => {
-      return response;
-    },
-  
-    (error) => {
-      if (error.response && error.response.status === 401) {
-        store.dispatch(logout());
-            window.location.href = "/volunteer/login";
-        
-      }
-  
-      return Promise.reject(new Error("Session expired. Please log in again."));
+  (response) => {
+    return response;
+  },
+
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      store.dispatch(logout());
+      window.location.href = "/volunteer/login";
     }
-  );
+
+    return Promise.reject(new Error("Session expired. Please log in again."));
+  }
+);
 
 export default volunteerApi;
