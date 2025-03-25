a5ee73eff4ac37f9acaa9735590d18a09afd19f4
DhishonThomas <dhishonthomas23@gmail.com>
2025-03-25 19:33:13 +0530

    Added favicon the log and changed the name.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,8 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Digi Colab",
+  title: "Anara",
+  icons: "/favicon.png"
 };
 
 export default function RootLayout({
