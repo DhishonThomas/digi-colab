4c9f4e7ee2429a975b83d09b5bb1ba0ed291a4d0
DhishonThomas <dhishonthomas23@gmail.com>
2025-05-02 13:09:05 +0530

    Added letterheads. and fixed many bugs.. working..

---
+++ src/app/(dashboard)/admin/letter-heads/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+
+const LetterHeads = () => {
+ 
+    return (
+        <div>
+                 <button
+          className="px-5 py-2 bg-[#B56365] text-white hover:bg-[#b56364f8]"
+        >
+          + Add Course
+        </button>
+        </div>
+    )
+};
+
+export default LetterHeads;
