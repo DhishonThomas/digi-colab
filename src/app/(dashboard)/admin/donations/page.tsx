5d88498c0e588793954353a990ef4be9a69b346e
DhishonThomas <dhishonthomas23@gmail.com>
2025-06-26 16:00:06 +0530

    Fixed issues .. on the donation part.

--- src/app/(dashboard)/admin/donations/page.tsx
+++ src/app/(dashboard)/admin/donations/page.tsx
@@ -129,7 +129,7 @@ const Page = () => {
                       >
                         View
                       </button>
-                    </td>
+                    </td> 
                   </tr>
                 ))}
               </tbody>
